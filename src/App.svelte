786a723b4da448abda469cf64ea2e2b386f0351a
Sami <sami@netmaking.no>
2020-03-02 08:52:55 +0100

    Added bootstrap and styling to table of repos

--- src/App.svelte
+++ src/App.svelte
@@ -1,4 +1,6 @@
 <script>
+    import Repo from './components/Repo.svelte';
+
     let greeting = 'Hello World';
     let repos = [];
     let Promise = fetchGithubData();
@@ -16,12 +18,53 @@
     }
 </script>
 
-{#await Promise}
-    <p>Fetching Repos ....</p>
-{:then repos}
+<div class="container">
+
+    {#await Promise}
+        <p>Fetching Repos ....</p>
+    {:then repos}
+    <table class="table">
+        <thead class="thead-dark">
+        <tr>
+            <th scope="col">Rank</th>
+            <th scope="col">Name</th>
+            <th scope="col">Stars</th>
+            <th scope="col">Avatar</th>
+            <th scope="col">Github</th>
+        </tr>
+        </thead>
+    <tbody>
     {#each repos as repo, i}
-        <h1> {repo.name} </h1>
+        <tr class="mt-2">
+            <th scope="row">
+                {i + 1}
+            </th>
+            <td>
+                {repo.name}
+            </td>
+            <td>
+                {repo.stargazers_count.toLocaleString() }
+            </td>
+            <td>
+                <img class="avatar" src="{repo.owner.avatar_url}" alt="Repo Avatar" />
+            </td>
+            <td>
+                <a href="{repo.html_url}" target="_blank">Link</a>
+            </td>
+        </tr>
     {/each}
-{:catch error}
-    <h1 style="color: red;">{ error.message }</h1>
-{/await}
+    </tbody>
+
+    </table>
+
+    {:catch error}
+        <h1 style="color: red;">{ error.message }</h1>
+    {/await}
+</div>
+
+<style>
+    .avatar {
+        max-width: 150px;
+        max-height: 75px;
+    }
+</style>
